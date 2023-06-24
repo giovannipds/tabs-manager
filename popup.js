879b21358b544242c505da087fc9b98150b1886f
Giovanni Pires <giovannipds@gmail.com>
2023-06-23 23:46:21 -0300

    implement a group for tableless site

--- popup.js
+++ popup.js
@@ -1,9 +1,14 @@
-const tabs = await chrome.tabs.query({
+const chromeTabs = await chrome.tabs.query({
   url: [
     "https://developer.chrome.com/docs/webstore/*",
     "https://developer.chrome.com/docs/extensions/*"
   ]
 });
+const tablelessTabs = await chrome.tabs.query({
+  url: ["https://tableless.com.br/*"],
+});
+
+const tabs = [...chromeTabs, ...tablelessTabs];
 
 const collator = new Intl.Collator();
 tabs.sort((a, b) => collator.compare(a.title, b.title));
@@ -14,7 +19,8 @@ for (const tab of tabs) {
   const element = template.content.firstElementChild.cloneNode(true);
 
   const title = tab.title.split("-")[0].trim();
-  const pathname = new URL(tab.url).pathname.slice("/docs".length);
+  const url = new URL(tab.url);
+  const pathname = url.host.includes('chrome') ? url.pathname.slice("/docs".length) : url.pathname;
 
   element.querySelector(".title").textContent = title;
   element.querySelector(".pathname").textContent = pathname;
@@ -30,7 +36,19 @@ document.querySelector("ul").append(...elements);
 
 const button = document.querySelector("button");
 button.addEventListener("click", async () => {
-  const tabIds = tabs.map(({ id }) => id);
-  const group = await chrome.tabs.group({ tabIds });
-  await chrome.tabGroups.update(group, { color: "pink", title: "EXTENSION DOCS" });
+  const byId = ({ id }) => id;
+  const chromeTabIds = tabs
+    .filter((tab) => tab.url.includes('chrome'))
+    .map(byId);
+  const tablelessTabIds = tabs
+    .filter((tab) => tab.url.includes('tableless'))
+    .map(byId);
+  if (chromeTabIds.length) {
+    const group1 = await chrome.tabs.group({ tabIds: chromeTabIds });
+    await chrome.tabGroups.update(group1, { color: "pink", title: "EXTENSION DOCS" });
+  }
+  if (tablelessTabIds.length) {
+    const group2 = await chrome.tabs.group({ tabIds: tablelessTabIds });
+    await chrome.tabGroups.update(group2, { color: "red", title: "TABLELESS ROCKS" });
+  }
 })
